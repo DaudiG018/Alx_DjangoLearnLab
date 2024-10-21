a6164cdfc27128f7fde7a57cac3c8a00f37def0c
David Njoroge <davidnjoroge6598@gmail.com>
2024-10-21 20:17:49 +0300

    added inventory_project

--- inventory_project/inventory_project/settings.py
+++ inventory_project/inventory_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
